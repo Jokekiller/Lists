499c183534cee45ca51f8e2c96c9c49c64326b80
Harry Robinson <41194@longroad.ac.uk>
2014-12-09 15:17:45 +0000

    Lists stuff

---
+++ Elminating the lowest and finding the highest value.py
@@ -0,0 +1,10 @@
+result = 0
+index = 0
+
+for index in range (4):
+    values = int(input("Enter a number: ")
+    index = index + 1
+    if result < values[index]:
+        result = values[index]
+    print result
+    
